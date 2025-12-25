25ace52e8397ec0bac6a200e16ac9c85fd9a7087
Filip Knezevic <filipknezevic369@gmail.com>
2025-12-25 18:53:10 +0100

    Klikom na odabrani automobil se prikazuje njegova slika u gornjem delu stranice, a ispod nje se popunjava sekcija sa podacima odabranog auta

--- auto-centar/src/components/Cars.jsx
+++ auto-centar/src/components/Cars.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, Link } from "react-router-dom";
 import { cars } from "../data/cars";
 
 const Cars = () => {
@@ -8,13 +8,14 @@ const Cars = () => {
     return (
         <div className="cars-list">
             {cars.map(car => (
-                <div
+                <Link
                     key={car.id}
+                    to={`/ponuda?id=${car.id}`}
                     className={`car-item ${selectedId === car.id ? "selected" : ""}`}
                 >
                     <img src={car.image} alt={car.model.name} />
                     <p>Već od {car.price.fromEUR}€</p>
-                </div>
+                </Link>
             ))}
         </div>
     );
